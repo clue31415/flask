38f636f4ceddb764bb25ad1f9f3a3bb0adadf3dd
clue31415 <clue31415@gmail.com>
2026-06-11 10:04:23 +0900

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+SERVICE_KEY = SERVICEKEY_ENV
+
+API_URL = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList"
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('./index.html')
+    drugs = []
+
+    if request.method == "POST":
+        keyword = request.form.get("keyword")
+
+        params ={'serviceKey' : SERVICE_KEY,
+         'pageNo' : '1',
+         'numOfRows' : '5',
+         'entpName' : '',
+         'itemName' : '',
+         'itemSeq' : '',
+         'efcyQesitm' : keyword,
+         'useMethodQesitm' : '',
+         'atpnWarnQesitm' : '',
+         'atpnQesitm' : '',
+         'intrcQesitm' : '',
+         'seQesitm' : '',
+         'depositMethodQesitm' : '',
+         'openDe' : '',
+         'updateDe' : '',
+         'type' : 'json' }
+
+        try:
+            response = requests.get(API_URL, params=params)
+            data = response.json()
+
+            items = (
+                data.get("body", {})
+                    .get("items", [])
+            )
+
+            drugs = items
+
+        except Exception as e:
+            print("에러:", e)
+
+    return render_template("index.html", drugs=drugs)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
